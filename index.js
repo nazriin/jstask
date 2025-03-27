221238a1780658f7d948d12ba2cde86e8bf71a19
nazriin <ibabazade014@gmail.com>
2025-03-27 11:03:38 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+
+// · 1-dən 100-ə qədər olan ədədlərin cəmini tapın.
+// let cem=0;
+// for (let i=0;i<=100;i++){
+    //     cem+=i;
+    // }
+    // console.log(cem);
+
+
+
+// · Verilmiş array: [1, 2, 3, 4, 5] – bu array-dəki bütün ədədləri konsola yazdırın.
+// const arr=[1,2,3,4,5];
+// let text='';
+// for (let i=0;i<arr.length;i++){
+//     text+=arr[i]+' ';
+// }
+// console.log(text)
+
+
+// · Verilmiş obyekt: {ad: 'Ali', yaş: 25, şəhər: 'Bakı'} – bu obyektin bütün açarlarını konsola yazdırın.
+// const man={
+//   ad:'Ali',
+//   yaş: 25,
+//   şəhər: 'Baki'
+// };
+// let text='';
+// for (let x in man){
+//   text+=man[x]+' '
+// }
+// console.log(text);
+
+
+// · Verilmiş obyekt: {kitab: '1984', müəllif: 'George Orwell'} – açar və dəyərləri konsola yazdırın.for of
+// const obj={kitab: '1984', müəllif: 'George Orwell'};
+// for (let i in obj){
+//   console.log(i + ' '+obj[i]);
+// }
+
+
+// · Verilmiş array: ['alma', 'armud', 'banan'] – bütün meyvələri konsola yazdırın.
+// const fruits=['alma', 'armud', 'banan'];
+// let text='';
+// for (let x of fruits){
+//   text+=x+' ';
+// }
+// console.log(text);
+
+
+// · Verilmiş string: 'Salam' – hər bir hərfi konsola yazdırın.while
+// let soz='salam';
+// for( let i of soz){
+//   console.log(i);
+// }
+
+
+// · 1-dən 10-a qədər olan ədədləri konsola yazdırın.
+// let text='';
+// let i=1;
+// while (i<=10){
+//   text+=i+' ';
+//   i++;
+// }
+// console.log(text);
+
+
+// · Verilmiş array: [5, 10, 15, 20] – array boşalana qədər elementləri konsola yazdırın.
+// let arr=[5,10,15,20];
+// let i=0;
+// while (i<arr.length){
+//   console.log(arr[i]);
+//   i++;
+// }
+
+
+// · 1-dən 5-ə qədər ədədləri do while ilə konsola yazdırın.
+// let text = ""
+// let i = 1;
+// do {
+//   text += i +' ';
+//   i++;
+// }
+// while (i <= 5);
+// console.log(text);
+
+
+
+// · Verilmiş şərt: istifadəçi ədəd daxil edir və ədəd 50-dən kiçikdirsə döngü davam edir.
+// let i = 40;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 50);
+
+
+
+
+
+  
